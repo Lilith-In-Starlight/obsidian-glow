90d75c76b4e3b533f01b016d856fdbc62097bbcc
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-07-26 20:01:39 -0500

    Fixed a bug where it was possible to save while not on a bench as long as a bench was present in the scene

--- Elements/Bench.gd
+++ Elements/Bench.gd
@@ -4,6 +4,6 @@ extends Sprite
 onready var Player := $"../../Neptune"
 
 func _process(delta):
-	if Player.move_up:
+	if Player.move_up and Player.position.distance_to(position) <  30:
 		Persistent.player_pos = Player.position
 		Persistent.save()

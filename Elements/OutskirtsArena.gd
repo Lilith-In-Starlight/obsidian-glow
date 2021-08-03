1ac92de629fab686f2718b484550f9ee7045c7ec
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-08-03 12:00:12 -0500

    More specific variable names

--- Elements/OutskirtsArena.gd
+++ Elements/OutskirtsArena.gd
@@ -1,37 +1,43 @@
 extends Node2D
 
-var closed = false
+onready var CollisionShapeLeft := $Barriers/CollisionShape2D
+onready var CollisionShapeRight := $Barriers/CollisionShape2D2
+onready var BarrierLeft := $Barriers/EnemyBarrier
+onready var BarrierRight := $Barriers/EnemyBarrier2
+onready var Barriers := $Barriers
+
+var closed := false
 
 var phase := 3
 
 func _ready():
 	if Persistent.outskirts_arena:
-		$Barriers/CollisionShape2D.position.y = -50
-		$Barriers/CollisionShape2D2.position.y = -50
+		CollisionShapeLeft.position.y = -50
+		CollisionShapeRight.position.y = -50
 
 func _process(delta):
 	if Persistent.outskirts_arena:
-		$Barriers/CollisionShape2D.position.y = lerp($Barriers/CollisionShape2D.position.y, -80, 0.2)
-		$Barriers/CollisionShape2D2.position.y = lerp($Barriers/CollisionShape2D2.position.y, -80, 0.2)
+		CollisionShapeLeft.position.y = lerp(CollisionShapeLeft.position.y, -80, 0.2)
+		CollisionShapeRight.position.y = lerp(CollisionShapeRight.position.y, -80, 0.2)
 	elif $"../Neptune".position.x < 209:
-		$Barriers/CollisionShape2D.position.y = lerp($Barriers/CollisionShape2D.position.y, 71, 0.2)
-		$Barriers/CollisionShape2D2.position.y = lerp($Barriers/CollisionShape2D2.position.y, 71, 0.2)
+		CollisionShapeLeft.position.y = lerp(CollisionShapeLeft.position.y, 71, 0.2)
+		CollisionShapeRight.position.y = lerp(CollisionShapeRight.position.y, 71, 0.2)
 		if !closed:
 			closed = true
-			var asdfa := preload("res://Enemies/DashAngel.tscn")
-			var asdf := asdfa.instance()
-			add_child(asdf)
-			asdf.position = Vector2(0, 40)
+			var dash_angel := preload("res://Enemies/DashAngel.tscn")
+			var DashAngel := dash_angel.instance()
+			add_child(DashAngel)
+			DashAngel.position = Vector2(0, 40)
 			$"../Neptune".shake = 1.0
 	elif $"../Neptune".position.x > 209:
-		$Barriers/CollisionShape2D.position.y = lerp($Barriers/CollisionShape2D.position.y, 71, 0.2)
-		$Barriers/CollisionShape2D2.position.y = lerp($Barriers/CollisionShape2D2.position.y, -80, 0.2)
-	$Barriers/EnemyBarrier.position.y = $Barriers/CollisionShape2D.position.y - 2
-	$Barriers/EnemyBarrier2.position.y = $Barriers/CollisionShape2D2.position.y + 10
+		CollisionShapeLeft.position.y = lerp(CollisionShapeLeft.position.y, 71, 0.2)
+		CollisionShapeRight.position.y = lerp(CollisionShapeRight.position.y, -80, 0.2)
+	BarrierLeft.position.y = CollisionShapeLeft.position.y - 2
+	BarrierRight.position.y = CollisionShapeRight.position.y + 10
 	
 	var health := 0
 	for i in get_children():
-		if i != $Barriers:
+		if i != Barriers:
 			if i.health > 0:
 				health += i.health
 	if health <= 0:

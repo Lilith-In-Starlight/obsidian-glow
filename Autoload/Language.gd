885d96c915d025b533ac734cfa67f7abba3ee865
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-08-04 16:01:35 -0500

    Extended the spike garden and created the spike guardians

--- Autoload/Language.gd
+++ Autoload/Language.gd
@@ -9,6 +9,8 @@ var lines := {
 		"diary_angel" : "Angels:\n\n  These guys are everywhere. They prepare to shoot way before they actually do it, which makes them extremely annoying. The only reason this thing killed  me is that I was stabbed.",
 		"diary_angel_ball" : "Ball Angels:\n\n  These ones bounce around mindlessly. Once they see a target, they lit themselves on fire and charge towards them.",
 		"diary_gate_outskirts" : "Oh right. I saw this weird... wall? thing near the outskirts of the city. The symbols in it were incomprehensible, I wonder what it is or what it's behind.\n\nit calls to me...",
+		"diary_spike_guardian": "Spike Guardians:\n\n  These people seem to be hunters. They look like the Librarian, so I assume they are Spike Gardeners. This must be their garden. I didn't expect 'gardeners' to be more like hunters. They are also less mindless than angels, which is fun.",
+		"diary_spike_guardian_magic": "Spike Guardians 2:\n\n Yep. They are magicians. I have to be careful with how often I attack, or they'll use their magic against me, so that's good to know."
 	},
 }
 
@@ -16,3 +18,8 @@ func line(line):
 	if lines[lang].has(line):
 		return lines[lang][line]
 	return "null"
+
+func not_in_diary(line):
+	if not Persistent.diary.has(line) and not Persistent.recently_collected.has(line):
+		return true
+	return false
--- Enemies/SpikeGuardian.gd
+++ Enemies/SpikeGuardian.gd
@@ -1,10 +1,159 @@
-extends Node2D
+extends KinematicBody2D
 
 
-enum states {
-	IDLE,
-	CHARGING,
-	SLASHING,
-	DEFENDING,
-	BREATHING,
+enum STATES {
+	IDLE, # Hasn't seen the player
+	CHARGING, # Saw the player and is ready to attack
+	SLASHING, # Attacking
+	DEFENDING, # The player's getting greedy with attacks
+	BREATHING, # Has a random chance of getting tired for a short time
+	DEAD,
 }
+
+onready var Animations := $Animations
+onready var Cast := $RayCast
+
+var Player
+var current_state = STATES.IDLE
+var speed := Vector2(0, 0)
+
+var health := 8
+
+var attacks_while_defending := 0
+var time_since_last_damage := 0.0
+
+func _ready():
+	if Persistent.killed.has([get_tree().current_scene.filename, name]):
+		queue_free()
+	Player = get_tree().get_nodes_in_group("player")[0]
+
+
+func _physics_process(delta):
+	Cast.cast_to = Player.position - position
+	if health <= 0:
+		current_state = STATES.DEAD
+	time_since_last_damage += delta
+	match current_state:
+		STATES.IDLE:
+			$Hurtbox/CollisionShape2D2.disabled = true
+			$SpikeAttack.modulate.a -= 0.1
+			speed.x *= 0.75
+			Animations.play("default")
+			if Cast.is_colliding() and Cast.get_collider() == Player and Player.position.distance_to(position) < 100:
+				current_state = STATES.SLASHING
+			
+			if Player.position.x > position.x:
+				Animations.scale.x = -1
+			else:
+				Animations.scale.x = 1
+		
+		STATES.CHARGING:
+			pass
+		STATES.SLASHING:
+			$Hurtbox/CollisionShape2D2.disabled = true
+			$SpikeAttack.modulate.a -= 0.1
+			Animations.play("slashing")
+		STATES.DEFENDING:
+			speed.x *= 0.75
+			
+			if Player.position.x > position.x:
+				Animations.scale.x = -1
+			else:
+				Animations.scale.x = 1
+				
+			if Player.position.y < position.y - 10:
+				Animations.play("defend_v")
+			else:
+				Animations.play("defend_h")
+			
+			if attacks_while_defending >= 3:
+				if $SpikeAttack.modulate.a < 0:
+					$SpikeAttack.modulate.a = 0
+				$SpikeAttack.modulate.a += 0.01
+				if $SpikeAttack.modulate.a >= 1.0:
+					$Hurtbox/CollisionShape2D2.disabled = false
+					if Language.not_in_diary("diary_spike_guardian"):
+						Persistent.recently_collected.append("diary_spike_guardian")
+					if Language.not_in_diary("diary_spike_guardian_magic"):
+						Persistent.recently_collected.append("diary_spike_guardian_magic")
+					current_state = STATES.IDLE
+			else:
+				if time_since_last_damage > 0.7:
+					attacks_while_defending = 0
+					current_state = STATES.IDLE
+			
+				
+		STATES.BREATHING:
+			pass
+		STATES.DEAD:
+			$Hurtbox/CollisionShape2D2.disabled = true
+			$SpikeAttack.modulate.a -= 0.1
+			$Hurtbox/CollisionShape2D.disabled = true
+			speed.x *= 0.75
+			Animations.play("dead")
+	
+	speed.y += 100
+	
+	if is_on_floor():
+		speed.y = 60
+	
+	match Animations.animation:
+		"default":
+			Animations.position = Vector2(0, 0)
+		"slashing":
+			Animations.position = Vector2(-7 * Animations.scale.x, 0)
+		"dead":
+			Animations.position = Vector2(-2 * Animations.scale.x, 0)
+		"defend_h":
+			Animations.position = Vector2(-4 * Animations.scale.x, 1)
+		"defend_v":
+			Animations.position = Vector2(-2 * Animations.scale.x, -3)
+	
+	speed = move_and_slide(speed, Vector2.UP)
+
+
+func _on_animation_finished():
+	match Animations.animation:
+		"slashing":
+			if current_state != STATES.DEAD:
+				current_state = STATES.IDLE
+
+
+func _on_frame_changed():
+	match Animations.animation:
+		"slashing":
+			match Animations.frame:
+				2, 3:
+					$Hurtbox/CollisionShape2D.disabled = false
+					$Hurtbox/CollisionShape2D.position.x = -8 * Animations.scale.x
+					speed.x = -400 * Animations.scale.x
+					speed.y = 0
+				4:
+					$Hurtbox/CollisionShape2D.disabled = false
+					speed.x = 0
+
+
+func attacked(d, pos, s):
+	if time_since_last_damage > 0.7:
+		if health > 0:
+			Persistent.shadow += 2.0 + randf()*2.0
+		speed = (position-pos).normalized()*200 + s
+		var prev_health := health
+		health -= d
+		if health <= 0 and prev_health > 0:
+			Persistent.killed.append([get_tree().current_scene.filename, name])
+			if Language.not_in_diary("diary_spike_guardian"):
+				Persistent.recently_collected.append("diary_spike_guardian")
+				print(Persistent.recently_collected)
+			Persistent.persands += randi() % 5
+			queue_free()
+	else:
+		current_state = STATES.DEFENDING
+		attacks_while_defending += 1
+	
+	time_since_last_damage = 0
+
+
+func _on_Hurtbox_body_entered(body):
+	if body.name == "Neptune":
+		body.call("attacked", 1, position, speed.normalized()*110)

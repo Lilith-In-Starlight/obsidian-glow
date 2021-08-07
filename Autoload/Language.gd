36b0610739463214111b6c9133e398cc6665ec36
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-08-06 19:41:17 -0500

    Changed the tone in the first diary entries

--- Autoload/Language.gd
+++ Autoload/Language.gd
@@ -5,7 +5,7 @@ var lang := "en"
 
 var lines := {
 	"en": {
-		"diary_opening" : "So that old lady gave me this. Uh...\n\n  Dear diary? I have no idea what to write about here, but she asked me to, so here I am. She said she wanted knowledge, so I'll write about things I see",
+		"diary_opening" : "That old lady gave me this. I have no idea what to write about here. I'll write about things I see and then bring it back to her. It'll be useful, anyways.",
 		"diary_angel" : "Angels:\n\n  These guys are everywhere. They prepare to shoot way before they actually do it, which makes them extremely annoying. The only reason this thing killed  me is that I was stabbed.",
 		"diary_angel_ball" : "Ball Angels:\n\n  These ones bounce around mindlessly. Once they see a target, they lit themselves on fire and charge towards them.",
 		"diary_gate_outskirts" : "Oh right. I saw this weird... wall? thing near the outskirts of the city. The symbols in it were incomprehensible, I wonder what it is or what it's behind.\n\nit calls to me...",
